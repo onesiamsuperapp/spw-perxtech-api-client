03b3560e533eac634ed97975f0a70819bb5702e7
Satana Charuwichitratana <satana@xponential.asia>
2023-07-25 17:08:05 +0700

    fix: integration tests

--- jest-integration.config.ts
+++ jest-integration.config.ts
@@ -1,15 +1,13 @@
-import type { Config } from '@jest/types'
+import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testTimeout: 300000,
   testMatch: [
-    '<rootDir>/integration/**/**.test.ts',
+    '<rootDir>/integration/**/**.test.ts'
   ],
-  setupFiles: [
-    'dotenv/config',
-  ]
-}
+  setupFiles: ['dotenv/config'],
+};
 
-export default config
+export default config;
