e7f849bff791fce187aecdc5eca233c14d7996ac
Satana Charuwichitratana <satana@xponential.asia>
2023-07-25 12:18:03 +0700

    chore: add coverage threshold

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,16 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['<rootDir>/src/**/__tests__/**.test.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/**/index.ts'],
+  /* TODO: Increase when ready to do coverage */
+  coverageThreshold: {
+    global: {
+      statements: 54.57,
+      branches: 6.89,
+      functions: 11.67,
+      lines: 51.84,
+    },
+  },
 };
 
 export default config;
