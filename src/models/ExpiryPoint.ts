8deb0a9e952f64bb04635587b3c7f95d08c8d681
komsankk <komsan.k@siampiwat.com>
2024-08-05 17:55:45 +0700

    fix: handle support field response from perx

--- src/models/ExpiryPoint.ts
+++ src/models/ExpiryPoint.ts
@@ -4,13 +4,17 @@ import { ISODateTimeSerializer } from '../utils/cerialize';
 /**
  *  {
       "date": "2023-04-01",
-      "aggregated_points": 0
+      "aggregated_points": 0,
+      "timestamp": "2023-04-01"
     }
  */
 export class ExpiryPoint {
   @autoserializeAs(ISODateTimeSerializer, 'date')
-  date: Date | null = null
+  date?: Date | null = null
 
   @autoserializeAs("aggregated_points")
   aggregatedPoints!: number;
+
+  @autoserializeAs(ISODateTimeSerializer, 'timestamp')
+  timestamp?: Date | null = null
 }
