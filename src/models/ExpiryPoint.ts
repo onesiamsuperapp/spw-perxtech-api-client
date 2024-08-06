1c360dc33634c7dcc1364fb9ad254bede1d7217c
komsankk <115126285+komsankk@users.noreply.github.com>
2024-08-06 11:33:33 +0700

    fix(SP-8992): handle support field response from perx
    Ref: https://siampiwat.atlassian.net/browse/SP-8992

--- src/models/ExpiryPoint.ts
+++ src/models/ExpiryPoint.ts
@@ -2,15 +2,20 @@ import { autoserializeAs } from 'cerialize';
 import { ISODateTimeSerializer } from '../utils/cerialize';
 
 /**
+ * หลังจาก deploy จะต้องเปลี่ยนจาก date ไปใช้ "timestamp" เนื่องจาก perx ทำการแก้เส้นเดิมไม่มี v2
  *  {
       "date": "2023-04-01",
-      "aggregated_points": 0
+      "aggregated_points": 0,
+      "timestamp": "2023-04-01"
     }
  */
 export class ExpiryPoint {
   @autoserializeAs(ISODateTimeSerializer, 'date')
-  date: Date | null = null
+  date?: Date | null = null
 
   @autoserializeAs("aggregated_points")
   aggregatedPoints!: number;
+
+  @autoserializeAs(ISODateTimeSerializer, 'timestamp')
+  timestamp?: Date | null = null
 }
